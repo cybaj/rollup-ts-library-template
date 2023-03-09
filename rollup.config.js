0f256486f798866c41e3efaa8326ff40e26b3014
cybaj <cybaj@naver.com>
2023-03-09 18:42:17 +0900

    update module resolver

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,30 @@
-import typescript from '@rollup/plugin-typescript'
-import resolve from '@rollup/plugin-node-resolve';
-import babel from '@rollup/plugin-babel';
+import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import common from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
 
 export default {
-  input: 'src/main.ts',
+  input: "src/main.ts",
   output: [
     {
-      file: 'dist/index.cjs.js',
-      format: 'cjs'
+      file: "dist/index.cjs.js",
+      format: "cjs",
     },
     {
-      file: 'dist/index.esm.js',
-      format: 'esm'
-    }
+      file: "dist/index.esm.js",
+      format: "esm",
+      sourcemap: true,
+      inlineDynamicImports: true,
+    },
   ],
   plugins: [
-    resolve(),
-    babel({babelHelpers: 'bundled'}),
+    resolve({browser: true}),
+    common(),
+    babel({ babelHelpers: "bundled" }),
     typescript({
       declaration: true,
-      declarationDir: 'dist/types'
-    })
-  ]
-}
+      declarationDir: "dist/types",
+    }),
+  ],
+};
 
